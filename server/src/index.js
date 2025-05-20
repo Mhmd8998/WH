ee5733e29b8e93dd90aacda93f91be92499d02bf
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-05-21 00:39:58 +0300

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ const { createTable: createWithdrawalTable } = require('./model/Withdrawl');
   try {
     await createUserTable();
     console.log('User table created');
-    
+
     await createProductTable();
     console.log('Product table created');
 
@@ -18,31 +18,29 @@ const { createTable: createWithdrawalTable } = require('./model/Withdrawl');
   }
 })();
 
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
 
 const Auth = require("./router/Auth");
 const Product = require("./router/Product");
-const Withdrawl = require('./router/Withdrawl');
+const Withdrawl = require("./router/Withdrawl");
 const Admin = require("./router/Admin");
-const WeeklyReport = require('./router/WeeklyReport');
+const WeeklyReport = require("./router/WeeklyReport");
 
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000" }));
 
-app.use(express.json())
-app.use(cors({origin: "http://localhost:3000"}))
+app.use("/api/auth", Auth);
+app.use("/api/product", Product);
+app.use("/api/withdrawl", Withdrawl);
+app.use("/api/admin", Admin);
+app.use("/api", WeeklyReport);
 
-app.use("/api/auth",Auth);
-app.use("/api/product",Product);
-app.use("/api/withdrawl",Withdrawl);
-app.use("/api/admin",Admin);
-app.use('/api', WeeklyReport);
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-app.get("/",(req,res)=>{
-    res.send("hello world")
-})
-
-app.listen(process.env.PORT,()=>{
-    console.log("start");
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
+});
